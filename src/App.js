407fc95ddc9220d02e0816e46196bf50ae1b3a48
SdNaqeeb <131010024+SdNaqeeb@users.noreply.github.com>
2024-03-02 21:44:23 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import { ChakraProvider } from "@chakra-ui/react";
+import Header from "./components/Header";
+import LandingSection from "./components/LandingSection";
+import ProjectsSection from "./components/ProjectsSection";
+import ContactMeSection from "./components/ContactMeSection";
+import Footer from "./components/Footer";
+import { AlertProvider } from "./context/alertContext";
+import Alert from "./components/Alert";
+import pic from "./images/pic.jpg"
+
+function App() {
+  return (
+    <ChakraProvider>
+      <AlertProvider>
+        <main>
+          <Header />
+          <LandingSection img={pic}/>
+          <ProjectsSection />
+          <ContactMeSection />
+          <Footer />
+          <Alert />
+        </main>
+      </AlertProvider>
+    </ChakraProvider>
+  );
+}
+
+export default App;
---
+++ src/components/LandingSection.js
@@ -0,0 +1,32 @@
+import React from "react";
+import { Avatar, Heading, VStack } from "@chakra-ui/react";
+import FullScreenSection from "./FullScreenSection";
+
+
+const greeting = "Hello, This is Syed Naqeeb!";
+const bio1 = "A frontend developer";
+const bio2 = "specialised in React";
+
+// Implement the UI for the LandingSection component according to the instructions.
+// Use a combination of Avatar, Heading and VStack components.
+const LandingSection = ({img}) => (
+  <FullScreenSection
+    justifyContent="center"
+    alignItems="center"
+    isDarkBackground
+    backgroundColor="#2A4365"
+  >
+    <VStack>
+      <Avatar
+        size="2xl"
+        name="Syed Naqeeb"
+        src={img}
+      />
+      <p>{greeting}</p>
+      <Heading>{bio1}</Heading>
+      <Heading>{bio2}</Heading>
+    </VStack>
+  </FullScreenSection>
+);
+
+export default LandingSection;
